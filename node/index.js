e2039c808878257aee1750f3b6bf31d5598c57d7
p.leroux <pleroux@student.42.fr>
2018-09-10 18:26:31 +0200

    ongoing

--- node/index.js
+++ node/index.js
@@ -6,35 +6,62 @@ const models  = require('./models');
   const page = await browser.newPage();
   await page.goto('https://www.flashscore.com/tennis/');
   await page.setViewport({width: 1366, height: 768});
+
+  // GET all tournament URL
   const elements = await page.evaluate(() => {
     let elements = Array.from(document.querySelector("li#lmenu_5724").querySelector("ul.submenu").querySelectorAll("a"));
     let links = elements.map(element => {
-      return [element.href, element.innerText ]
+      return [ element.href, element.innerText ]
     });
     return links;
   })
 
+  // Create jsondb Object
+  let jsondb = {};
+  jsondb.dataset = [];
+  jsondb.date = Date.now();
+
+  // While on each tournament 
   for (let i in elements) {
-    let tournamentName = elements[i][1];
+    let tmpTour = {};
+    tmpTour.tournamentName = elements[i][1];
+    tmpTour.tournamentUrl = elements[i][0];
+    tmpTour.tour = [];
+    
+    // ICI exporter la creation des donnees dans la base a la fin de la generation du json
+    // Save le Json dans un fichier a la fin
+
+    // Save on tournament DB TABLE
     // console.log(elements[i][0], elements[i][1]);
     // var event = await models.tournament.create({
     //   flashscoreUrl: elements[i][0],
     //   name: tournamentName
     // });
     // let tournamentId = event.dataValues.id;
+    
+    // Get all URL per years
     await page.goto(elements[i][0] + 'archive/');
     const linkYears = await page.evaluate(() => {
       let elements = Array.from(document.querySelector(".tournament-page-archiv").querySelectorAll("a"));
       let links = elements.map((element, index) => {
-        return (index % 2 == 0 ? element.href : null);
+        return [ (index % 2 == 0 ? element.href : null),
+          /(19[5-9]\d|20[0-4]\d|2050)$/.exec(element.innerText)[0] ];
       });
       return links;
     });
 
+    // While on each tournament archive per year
     for (let j in linkYears) {
-      if (linkYears[j]) {
+      if (linkYears[j][0]) {
+        let tmpTourYear = {};
+        tmpTourYear.year = linkYears[j][1]; 
+
+        // Get Resultat table only
         await page.goto(linkYears[j] + 'results/');
-        const linkMatch = await page.evaluate(() => {
+        
+        //parse each <tr> in table
+        tmpTourYear.tourYear = await page.evaluate(() => {
+
           //display all match
           loadMoreGames();
           loadMoreGames();
@@ -61,14 +88,21 @@ const models  = require('./models');
           return retu;
         });
 
-        //Scrap uniquement les matchs
+        // Scrap all data on each match ID
         for (let k in linkMatch) {
           for (let kk in linkMatch[k].match) {
-            await page.goto("https://www.flashscore.com/match/" + linkMatch[k].match[kk])
+            await page.goto("https://www.flashscore.com/match/" + linkMatch[k].match[kk]);
+            await page.evaluate(() => {
+              detail_tab('odds-comparison');
+              detail_tab('statistics');
+              detail_tab('match-history');
+            })
           }
-        }
-      }
-    }
+        } // while match
+        tmpTour.tour.push(tmpTourYear);
+      } // while years
+    } // while years
+    jsondb.dataset.push(tmpTour);
   }
 
   await browser.close();
