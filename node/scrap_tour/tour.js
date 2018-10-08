bf920366be0f0b25f210c3aa30f8bf7cc505b1ee
pleroux <p.leroux@gmx.com>
2018-10-08 09:34:43 +0200

    add error handle tour

--- node/scrap_tour/tour.js
+++ node/scrap_tour/tour.js
@@ -36,112 +36,118 @@ async function getTour(tournamentName, tournamentUrl) {
   await page.goto(tournamentUrl + 'archive/');
 
   await page.waitFor(config.delay_waitForG); // wait for stabilization
-  const linkYears = await page.evaluate(ftour.linkYears);
 
-  // While on each tournament archive per year
-  for (let j in linkYears) {
-    if (linkYears[j]) {
-      var year = linkYears[j][1];
-      var linkTour = linkYears[j][0];
+  try {
+    const linkYears = await page.evaluate(ftour.linkYears);
 
-      // Get Resultat table only
-      await page.goto(linkTour + 'results/');
-      await page.waitFor(config.delay_waitForG); // wait for stabilization
-      //parse each <tr> in table
-      tourYear = await page.evaluate(ftour.tourYears);
+    // While on each tournament archive per year
+    for (let j in linkYears) {
+      if (linkYears[j]) {
+        var year = linkYears[j][1];
+        var linkTour = linkYears[j][0];
 
-      // Scrap all data on each match ID
-      for (let k in tourYear) {
-        // loop on match by competition on tournament (ex: qualification or not)
-        for (let kk in tourYear[k].match) {
-          var flashscoreId = tourYear[k].match[kk].id;
-          var matchUrl = config.match + flashscoreId;
-          // create new line Table HEAD and flashscore
-          // All the data is available at this point
+        // Get Resultat table only
+        await page.goto(linkTour + 'results/');
+        await page.waitFor(config.delay_waitForG); // wait for stabilization
+        //parse each <tr> in table
+        tourYear = await page.evaluate(ftour.tourYears);
 
-          try {
-            var db_head = await models.head.findOne({
-              where: {
-                flashscoreId: flashscoreId
-              }
-            });
+        // Scrap all data on each match ID
+        for (let k in tourYear) {
+          // loop on match by competition on tournament (ex: qualification or not)
+          for (let kk in tourYear[k].match) {
+            var flashscoreId = tourYear[k].match[kk].id;
+            var matchUrl = config.match + flashscoreId;
+            // create new line Table HEAD and flashscore
+            // All the data is available at this point
 
-            if (db_head == null || db_head.dataValues.stateFlashscore != "ok") {
-              try {
-                // scraping match
-                var jsonMatch = await match.getMatch(flashscoreId);
-                if (jsonMatch.state == "ERROR") {
-                  throw "JsonMatch is unvalid : " + jsonMatch.error;
-                }
-
-                var idHome = jsonMatch.player.home.playerID;
-                var UrlHome = config.rootUrl + jsonMatch.player.home.playerURL;
-                var home = await player.getPlayer(UrlHome, idHome, jsonMatch.player.home.playerCountry);
-                var idAway = jsonMatch.player.away.playerID;
-                var UrlAway = config.rootUrl + jsonMatch.player.away.playerURL;
-                var away = await player.getPlayer(UrlAway, idAway, jsonMatch.player.away.playerCountry);
-                if (home.state != "ok") {
-                  throw "getPlayer Home is unvalid :" + idHome + ": " + home.error
-                } else if (away.state != "ok") {
-                  throw "getPlayer Away is unvalid :" + idAway + ": " + away.error
-                }
-
-                var date = Date.UTC(year, tourYear[k].match[kk].month,
-                              tourYear[k].match[kk].day,
-                              tourYear[k].match[kk].hour,
-                              tourYear[k].match[kk].min);
-
-                var jsonWeather = await weather.setWeather(tournamentName,
-                                          tourYear[k].country, date);
-                if (jsonWeather.state == "ERROR") {
-                  throw "setWeather is unvalid " + weather.error;
-                }
-
-                //Update or Create in flashscore table
-                await dbTools.upsert("flashscore", {
-                  state: "ok",
-                  matchUrl: matchUrl,
-                  tournamentUrl: tournamentUrl,
-                  flashscoreId: flashscoreId,
-                  tournamentName: tournamentName,
-                  round: tourYear[k].match[kk].round,
-                  qualification: tourYear[k].qualification,
-                  indoor: tourYear[k].indoor,
-                  country: tourYear[k].country,
-                  surface: tourYear[k].surface,
-                  year: year,
-                  dateTime: date,
-                  data: jsonMatch,
-                }, {
+            try {
+              var db_head = await models.head.findOne({
+                where: {
                   flashscoreId: flashscoreId
-                });
-              } catch(e) {
-                res[flashscoreId] = e;
-                console.error("ERROR tour.js", e);
-                continue;
+                }
+              });
+
+              if (db_head == null || db_head.dataValues.stateFlashscore != "ok") {
+                try {
+                  // scraping match
+                  var jsonMatch = await match.getMatch(flashscoreId);
+                  if (jsonMatch.state == "ERROR") {
+                    throw "JsonMatch is unvalid : " + jsonMatch.error;
+                  }
+
+                  var idHome = jsonMatch.player.home.playerID;
+                  var UrlHome = config.rootUrl + jsonMatch.player.home.playerURL;
+                  var home = await player.getPlayer(UrlHome, idHome, jsonMatch.player.home.playerCountry);
+                  var idAway = jsonMatch.player.away.playerID;
+                  var UrlAway = config.rootUrl + jsonMatch.player.away.playerURL;
+                  var away = await player.getPlayer(UrlAway, idAway, jsonMatch.player.away.playerCountry);
+                  if (home.state != "ok") {
+                    throw "getPlayer Home is unvalid :" + idHome + ": " + home.error
+                  } else if (away.state != "ok") {
+                    throw "getPlayer Away is unvalid :" + idAway + ": " + away.error
+                  }
+
+                  var date = Date.UTC(year, tourYear[k].match[kk].month,
+                    tourYear[k].match[kk].day,
+                    tourYear[k].match[kk].hour,
+                    tourYear[k].match[kk].min);
+
+                    var jsonWeather = await weather.setWeather(tournamentName,
+                      tourYear[k].country, date);
+                      if (jsonWeather.state == "ERROR") {
+                        throw "setWeather is unvalid " + weather.error;
+                      }
+
+                      //Update or Create in flashscore table
+                      await dbTools.upsert("flashscore", {
+                        state: "ok",
+                        matchUrl: matchUrl,
+                        tournamentUrl: tournamentUrl,
+                        flashscoreId: flashscoreId,
+                        tournamentName: tournamentName,
+                        round: tourYear[k].match[kk].round,
+                        qualification: tourYear[k].qualification,
+                        indoor: tourYear[k].indoor,
+                        country: tourYear[k].country,
+                        surface: tourYear[k].surface,
+                        year: year,
+                        dateTime: date,
+                        data: jsonMatch,
+                      }, {
+                        flashscoreId: flashscoreId
+                      });
+                    } catch(e) {
+                      res[flashscoreId] = e;
+                      console.error("ERROR tour.js", e);
+                      continue;
+                    }
+                    //Update or Create in head table
+                    await dbTools.upsert("head", {
+                      flashscoreId: flashscoreId,
+                      homeId: idHome,
+                      stateHome: "ok",
+                      awayId: idAway,
+                      stateAway: "ok",
+                      stateFlashscore: "ok",
+                      weatherId: jsonWeather.hash,
+                      stateWeather: "ok"
+                    }, {
+                      flashscoreId: flashscoreId
+                    })
+                  } // IF db_head doesn't exist
+                } catch(e) {
+                  res[flashscoreId] = e;
+                  console.error("ERROR tour.js", e);
+                }
               }
-              //Update or Create in head table
-              await dbTools.upsert("head", {
-                flashscoreId: flashscoreId,
-                homeId: idHome,
-                stateHome: "ok",
-                awayId: idAway,
-                stateAway: "ok",
-                stateFlashscore: "ok",
-                weatherId: jsonWeather.hash,
-                stateWeather: "ok"
-              }, {
-                flashscoreId: flashscoreId
-              })
-            } // IF db_head doesn't exist
-          } catch(e) {
-            res[flashscoreId] = e;
-            console.error("ERROR tour.js", e);
-          }
-        }
-      }
-    } // while years
-  } // while years
+            }
+          } // while years
+        } // while years
+  } catch(e) {
+    res.error = e;
+    console.error("ERROR tour.js in", tournamentName, e);
+  }
 
   await browser.close();
   //await models.sequelize.close();
