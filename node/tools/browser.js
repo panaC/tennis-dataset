59514292a224efa873f6ea8cbe03eebe2f16d572
pleroux <p.leroux@gmx.com>
2018-10-10 09:15:35 +0200

    test try catch browser

--- node/tools/browser.js
+++ node/tools/browser.js
@@ -1,18 +1,21 @@
 const puppeteer   = require('puppeteer');
 const config      = require(__dirname + '/../config/config.js')["setting"];
 
-module.exports.browser = async function (url) {
-  const browser = await puppeteer.connect({
-    browserWSEndpoint: 'ws://' + config.browserless_ip + ':' + config.browserless_port,
-    defaultViewport: config.dim_screen
-  });
+module.exports.browser = async function browser (url) {
+  try {
+    var browser = await puppeteer.connect({
+      browserWSEndpoint: 'ws://' + config.browserless_ip + ':' + config.browserless_port,
+      defaultViewport: config.dim_screen
+    });
+
+    var page = await browser.newPage();
+    // Start URL
+    await page.goto(url);
+    await page.waitFor(config.delay_waitForG); // wait for stabilization
+  } catch(e) {
+    return await browser(url);
+  }
   //const browser = await browser_.createIncognitoBrowserContext();
-
-  var page = await browser.newPage();
-  // Start URL
-  await page.goto(url);
-  await page.waitFor(config.delay_waitForG); // wait for stabilization
-
   return page;
 };
 
