ccf29bbe1668a14380a3e741c11ecb4a9860e8ea
pleroux <p.leroux@gmx.com>
2018-10-21 15:16:55 +0200

    add scraping atpworldtour because tennis_atp lacked of data

---
+++ app/atpworldtour/index.js
@@ -0,0 +1,287 @@
+const scrapeIt    = require("scrape-it");
+const date        = require("date-and-time");
+const parsePrice  = require('parse-price')
+
+async function tour(year) {
+  return await scrapeIt("https://www.atpworldtour.com/en/scores/results-archive?year=" + year, {
+    [year]: {
+      listItem: "div#scoresResultsArchive tbody tr",
+      data: {
+        name: {
+          selector: "span.tourney-title",
+          trim: true
+        },
+        location: {
+          selector: "span.tourney-location",
+          trim: true
+        },
+        date: {
+          selector: "span.tourney-dates",
+          trim: true,
+          convert: x => new Date(date.parse(x, "YYYY.MM.DD", true))
+        },
+        door: {
+          selector: "td.tourney-details div.item-details",
+          trim: true,
+          eq: 1,
+          convert: x => x.split(" ")[0]
+        },
+        surface: {
+          selector: "td.tourney-details span.item-value",
+          trim: true,
+          eq: 2
+        },
+        prize: {
+          selector: "td.tourney-details.fin-commit span.item-value",
+          trim: true,
+          convert: x => parsePrice(x)
+        },
+        url: {
+          selector: "a.button-border",
+          attr: "href"
+        }
+      }
+    }
+  })
+}
+
+async function for_tour() {
+  let json_tour = {};
+  for (let i = 2001; i <= 2018; i++) {
+    let year = i.toString();
+    data = await tour(year);
+    json_tour[year] = data.data[year];
+  }
+  return json_tour;
+}
+
+async function match(url) {
+  return await scrapeIt(url, {
+    match: {
+      listItem: "div#scoresResultsContent tbody tr",
+      data: {
+        winner: {
+          selector: "td.day-table-name",
+          eq: 0,
+          trim: true,
+        },
+        winner_url: {
+          selector: "td.day-table-name a",
+          eq: 0,
+          trim: true,
+          attr: "href"
+        },
+        loser: {
+          selector: "td.day-table-name",
+          eq: 1,
+          trim: true,
+        },
+        loser_url: {
+          selector: "td.day-table-name a",
+          eq: 1,
+          trim: true,
+          attr: "href"
+        },
+        stats_url: {
+          selector: "td.day-table-score a",
+          attr: "href"
+        }
+      }
+    }
+  })
+}
+
+function tr_stat(x) {
+  return {
+    ptg: x.slice('%')[0],
+    over: /\((.*)\/(.*)\)/.exec(x)[1],
+    under: /\((.*)\/(.*)\)/.exec(x)[2]
+  };
+}
+
+async function stats(url) {
+  return await scrapeIt(url, {
+    stats: {
+      selector: "div#playerStatsContainer",
+      data: {
+        winner_serve_rating: {
+          selector: "td.match-stats-number-left",
+          eq: 0,
+          trim: true
+        },
+        winner_aces: {
+          selector: "td.match-stats-number-left",
+          eq: 1,
+          trim: true
+        },
+        winner_first_serve: {
+          selector: "td.match-stats-number-left",
+          eq: 2,
+          trim: true
+        },
+        winner_first_serve_point_won: {
+          selector: "td.match-stats-number-left",
+          eq: 3,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_second_serve_point_won: {
+          selector: "td.match-stats-number-left",
+          eq: 4,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_bp_saved: {
+          selector: "td.match-stats-number-left",
+          eq: 5,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_service_game_played: {
+          selector: "td.match-stats-number-left",
+          eq: 6,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_return_rating: {
+          selector: "td.match-stats-number-left",
+          eq: 7,
+          trim: true
+        },
+        winner_first_serve_return_point_won: {
+          selector: "td.match-stats-number-left",
+          eq: 8,
+          trim: true
+        },
+        winner_second_serve_return_point_won: {
+          selector: "td.match-stats-number-left",
+          eq: 9,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_bp_converted: {
+          selector: "td.match-stats-number-left",
+          eq: 10,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_return_games_played: {
+          selector: "td.match-stats-number-left",
+          eq: 11,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_service_point_won: {
+          selector: "td.match-stats-number-left",
+          eq: 12,
+          trim: true
+        },
+        winner_return_point_won: {
+          selector: "td.match-stats-number-left",
+          eq: 13,
+          trim: true,
+          convert: tr_stat
+        },
+        winner_total_point_won: {
+          selector: "td.match-stats-number-left",
+          eq: 14,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_serve_rating: {
+          selector: "td.match-stats-number-right",
+          eq: 0,
+          trim: true
+        },
+        loser_aces: {
+          selector: "td.match-stats-number-right",
+          eq: 1,
+          trim: true
+        },
+        loser_first_serve: {
+          selector: "td.match-stats-number-right",
+          eq: 2,
+          trim: true
+        },
+        loser_first_serve_point_won: {
+          selector: "td.match-stats-number-right",
+          eq: 3,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_second_serve_point_won: {
+          selector: "td.match-stats-number-right",
+          eq: 4,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_bp_saved: {
+          selector: "td.match-stats-number-right",
+          eq: 5,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_service_game_played: {
+          selector: "td.match-stats-number-right",
+          eq: 6,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_return_rating: {
+          selector: "td.match-stats-number-right",
+          eq: 7,
+          trim: true
+        },
+        loser_first_serve_return_point_won: {
+          selector: "td.match-stats-number-right",
+          eq: 8,
+          trim: true
+        },
+        loser_second_serve_return_point_won: {
+          selector: "td.match-stats-number-right",
+          eq: 9,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_bp_converted: {
+          selector: "td.match-stats-number-right",
+          eq: 10,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_return_games_played: {
+          selector: "td.match-stats-number-right",
+          eq: 11,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_service_point_won: {
+          selector: "td.match-stats-number-right",
+          eq: 12,
+          trim: true
+        },
+        loser_return_point_won: {
+          selector: "td.match-stats-number-right",
+          eq: 13,
+          trim: true,
+          convert: tr_stat
+        },
+        loser_total_point_won: {
+          selector: "td.match-stats-number-right",
+          eq: 14,
+          trim: true,
+          convert: tr_stat
+        },
+      }
+    }
+  });
+}
+
+if (typeof require != 'undefined' && require.main == module) {
+  // for_tour().then((data) => {
+  //   console.log(data);
+  // });
+  stats("https://www.atpworldtour.com/en/scores/2018/7480/MS001/match-stats").then((data) => {
+    console.log(data.data);
+  });
+}
--- app/common_opponent/query.sql
+++ app/common_opponent/query.sql
@@ -1,131 +1,243 @@
 SELECT
-'looser Baghdatis' as info,
-COUNT(*) as count,
-AVG(winner_rank) as wrank,
-AVG(loser_rank) as lrank,
-AVG(minutes) as minutes,
-AVG(l_ace) as ace,
-AVG(l_df) as df,
-AVG(l_svpt) as svpt,
-AVG("l_1stIn") as firstin,
-AVG("l_1stWon") as firstwon,
-AVG("l_2ndWon") as secondwon,
-AVG("l_SvGms") as svgms,
-AVG("l_bpSaved") as bpSaved,
-AVG("l_bpFaced") as bpFaced from csvs
-WHERE csvs.winner_name in (
-	SELECT Result FROM
-	(
-		select csvs.winner_name as Result from csvs
-		where csvs.loser_name = 'Marcos Baghdatis'
+'surface & year' as info,
+count as count_total,
+first_value(wage) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(wage) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as wage,
+first_value(lage) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(lage) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as lage,
+first_value(count) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(count) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as count,
+first_value(wrank) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(wrank) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as wrank,
+first_value(lrank) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(lrank) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as lrank,
+first_value(minutes) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(minutes) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as minutes,
+first_value(ace) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(ace) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as ace,
+first_value(df) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(df) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as df,
+first_value(svpt) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(svpt) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as svpt,
+first_value(firstin) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(firstin) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as firstin,
+first_value(firstwon) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(firstwon) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as firstwon,
+first_value(secondwon) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(secondwon) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as secondwon,
+first_value(svgms) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(svgms) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as svgms,
+first_value(bpsaved) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(bpsaved) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as bpsaved,
+first_value(bpfaced) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING)
+	- last_value(bpfaced) over (ROWS BETWEEN UNBOUNDED PRECEDING AND UNBOUNDED FOLLOWING) as bpfaced
+FROM (
+	SELECT
+	AVG(wage) as wage,
+	AVG(lage) as lage,
+	SUM (count) as count,
+	AVG(wrank) as wrank,
+	AVG(lrank) as lrank,
+	AVG(minutes) as minutes,
+	AVG(ace) as ace,
+	AVG(df) as df,
+	AVG(svpt) as svpt,
+	AVG("firstin") as firstin,
+	AVG("firstwon") as firstwon,
+	AVG("secondwon") as secondwon,
+	AVG("svgms") as svgms,
+	AVG("bpsaved") as bpsaved,
+	AVG("bpfaced") as bpfaced
+	FROM (
+		SELECT
+		'looser Baghdatis' as info,
+		stddev(winner_age) as wage,
+		stddev(loser_age) as lage,
+		COUNT(*) as count,
+		stddev(winner_rank) as wrank,
+		stddev(loser_rank) as lrank,
+		AVG(minutes) as minutes,
+		AVG(l_ace) as ace,
+		AVG(l_df) as df,
+		AVG(l_svpt) as svpt,
+		AVG("l_1stIn") as firstin,
+		AVG("l_1stWon") as firstwon,
+		AVG("l_2ndWon") as secondwon,
+		AVG("l_SvGms") as svgms,
+		AVG("l_bpSaved") as bpsaved,
+		AVG("l_bpFaced") as bpfaced from csvs
+		WHERE csvs.winner_name in (
+			SELECT Result FROM
+			(
+				select csvs.winner_name as Result from csvs
+				where csvs.loser_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+				union
+				select csvs.loser_name as Result from csvs
+				where csvs.winner_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+			)tt
+				where Result in (
+					select csvs.winner_name as Result2 from csvs
+					where csvs.loser_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+					union
+					select csvs.loser_name as Result2 from csvs
+					where csvs.winner_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+				)
+		) AND csvs.loser_name = 'Marcos Baghdatis'
 		union
-		select csvs.loser_name as Result from csvs
-		where csvs.winner_name = 'Marcos Baghdatis'
-	)tt
-		where Result in (
-			select csvs.winner_name as Result2 from csvs
-			where csvs.loser_name = 'Roger Federer'
-			union
-			select csvs.loser_name as Result2 from csvs
-			where csvs.winner_name = 'Roger Federer'
-		)
-) AND csvs.loser_name = 'Marcos Baghdatis'
-union
-SELECT
-'winner Baghdatis' as info,
-COUNT(*) as count,
-AVG(winner_rank) as wrank,
-AVG(loser_rank) as lrank,
-AVG(minutes) as minutes,
-AVG(w_ace) as ace,
-AVG(w_df) as df,
-AVG(w_svpt) as svpt,
-AVG("l_1stIn") as firstin,
-AVG("w_1stWon") as firstwon,
-AVG("w_2ndWon") as secondwon,
-AVG("w_SvGms") as svgms,
-AVG("w_bpSaved") as bpSaved,
-AVG("w_bpFaced") as bpFaced  from csvs
-WHERE csvs.loser_name in (
-	SELECT Result FROM
-	(
-		select csvs.winner_name as Result from csvs
-		where csvs.loser_name = 'Marcos Baghdatis'
+		SELECT
+		'winner Baghdatis' as info,
+		stddev(winner_age) as wage,
+		stddev(loser_age) as lage,
+		COUNT(*) as count,
+		stddev(winner_rank) as wrank,
+		stddev(loser_rank) as lrank,
+		AVG(minutes) as minutes,
+		AVG(w_ace) as ace,
+		AVG(w_df) as df,
+		AVG(w_svpt) as svpt,
+		AVG("l_1stIn") as firstin,
+		AVG("w_1stWon") as firstwon,
+		AVG("w_2ndWon") as secondwon,
+		AVG("w_SvGms") as svgms,
+		AVG("w_bpSaved") as bpsaved,
+		AVG("w_bpFaced") as bpfaced  from csvs
+		WHERE csvs.loser_name in (
+			SELECT Result FROM
+			(
+				select csvs.winner_name as Result from csvs
+				where csvs.loser_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+				union
+				select csvs.loser_name as Result from csvs
+				where csvs.winner_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+			)tt
+				where Result in (
+					select csvs.winner_name as Result2 from csvs
+					where csvs.loser_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+					union
+					select csvs.loser_name as Result2 from csvs
+					where csvs.winner_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+				)
+		) AND csvs.winner_name = 'Marcos Baghdatis'
+	)pp
+	union
+	SELECT
+	AVG(wage) as wage,
+	AVG(lage) as lage,
+	SUM(count) as count,
+	AVG(wrank) as wrank,
+	AVG(lrank) as lrank,
+	AVG(minutes) as minutes,
+	AVG(ace) as ace,
+	AVG(df) as df,
+	AVG(svpt) as svpt,
+	AVG("firstin") as firstin,
+	AVG("firstwon") as firstwon,
+	AVG("secondwon") as secondwon,
+	AVG("svgms") as svgms,
+	AVG("bpsaved") as bpsaved,
+	AVG("bpfaced") as bpfaced
+	FROM (
+		SELECT
+		'loose Federer' as info,
+		stddev(winner_age) as wage,
+		stddev(loser_age) as lage,
+		COUNT(*) as count,
+		stddev(winner_rank) as wrank,
+		stddev(loser_rank) as lrank,
+		AVG(minutes) as minutes,
+		AVG(l_ace) as ace,
+		AVG(l_df) as df,
+		AVG(l_svpt) as svpt,
+		AVG("l_1stIn") as firstin,
+		AVG("l_1stWon") as firstwon,
+		AVG("l_2ndWon") as secondwon,
+		AVG("l_SvGms") as svgms,
+		AVG("l_bpSaved") as bpsaved,
+		AVG("l_bpFaced") as bpfaced from csvs
+		WHERE csvs.winner_name in (
+			SELECT Result FROM
+			(
+				select csvs.winner_name as Result from csvs
+				where csvs.loser_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+				union
+				select csvs.loser_name as Result from csvs
+				where csvs.winner_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+			)tt
+				where Result in (
+					select csvs.winner_name as Result2 from csvs
+					where csvs.loser_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+					union
+					select csvs.loser_name as Result2 from csvs
+					where csvs.winner_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+				)
+		) AND csvs.loser_name = 'Roger Federer'
 		union
-		select csvs.loser_name as Result from csvs
-		where csvs.winner_name = 'Marcos Baghdatis'
-	)tt
-		where Result in (
-			select csvs.winner_name as Result2 from csvs
-			where csvs.loser_name = 'Roger Federer'
-			union
-			select csvs.loser_name as Result2 from csvs
-			where csvs.winner_name = 'Roger Federer'
-		)
-) AND csvs.winner_name = 'Marcos Baghdatis'
-union
-SELECT
-'loose Federer' as info,
-COUNT(*) as count,
-AVG(winner_rank) as wrank,
-AVG(loser_rank) as lrank,
-AVG(minutes) as minutes,
-AVG(l_ace) as ace,
-AVG(l_df) as df,
-AVG(l_svpt) as svpt,
-AVG("l_1stIn") as firstin,
-AVG("l_1stWon") as firstwon,
-AVG("l_2ndWon") as secondwon,
-AVG("l_SvGms") as svgms,
-AVG("l_bpSaved") as bpSaved,
-AVG("l_bpFaced") as bpFaced from csvs
-WHERE csvs.winner_name in (
-	SELECT Result FROM
-	(
-		select csvs.winner_name as Result from csvs
-		where csvs.loser_name = 'Marcos Baghdatis'
-		union
-		select csvs.loser_name as Result from csvs
-		where csvs.winner_name = 'Marcos Baghdatis'
-	)tt
-		where Result in (
-			select csvs.winner_name as Result2 from csvs
-			where csvs.loser_name = 'Roger Federer'
-			union
-			select csvs.loser_name as Result2 from csvs
-			where csvs.winner_name = 'Roger Federer'
-		)
-) AND csvs.loser_name = 'Roger Federer'
-union
-SELECT
-'winner Federer' as info,
-COUNT(*) as count,
-AVG(winner_rank) as wrank,
-AVG(loser_rank) as lrank,
-AVG(minutes) as minutes,
-AVG(w_ace) as ace,
-AVG(w_df) as df,
-AVG(w_svpt) as svpt,
-AVG("l_1stIn") as firstin,
-AVG("w_1stWon") as firstwon,
-AVG("w_2ndWon") as secondwon,
-AVG("w_SvGms") as svgms,
-AVG("w_bpSaved") as bpSaved,
-AVG("w_bpFaced") as bpFaced  from csvs
-WHERE csvs.loser_name in (
-	SELECT Result FROM
-	(
-		select csvs.winner_name as Result from csvs
-		where csvs.loser_name = 'Marcos Baghdatis'
-		union
-		select csvs.loser_name as Result from csvs
-		where csvs.winner_name = 'Marcos Baghdatis'
-	)tt
-		where Result in (
-			select csvs.winner_name as Result2 from csvs
-			where csvs.loser_name = 'Roger Federer'
-			union
-			select csvs.loser_name as Result2 from csvs
-			where csvs.winner_name = 'Roger Federer'
-		)
-) AND csvs.winner_name = 'Roger Federer'
+		SELECT
+		'winner Federer' as info,
+		stddev(winner_age) as wage,
+		stddev(loser_age) as lage,
+		COUNT(*) as count,
+		stddev(winner_rank) as wrank,
+		stddev(loser_rank) as lrank,
+		AVG(minutes) as minutes,
+		AVG(w_ace) as ace,
+		AVG(w_df) as df,
+		AVG(w_svpt) as svpt,
+		AVG("l_1stIn") as firstin,
+		AVG("w_1stWon") as firstwon,
+		AVG("w_2ndWon") as secondwon,
+		AVG("w_SvGms") as svgms,
+		AVG("w_bpSaved") as bpsaved,
+		AVG("w_bpFaced") as bpfaced  from csvs
+		WHERE csvs.loser_name in (
+			SELECT Result FROM
+			(
+				select csvs.winner_name as Result from csvs
+				where csvs.loser_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+				union
+				select csvs.loser_name as Result from csvs
+				where csvs.winner_name = 'Marcos Baghdatis' AND
+				csvs.tourney_date ~ '^2018' AND
+				csvs.surface = 'Clay'
+			)tt
+				where Result in (
+					select csvs.winner_name as Result2 from csvs
+					where csvs.loser_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+					union
+					select csvs.loser_name as Result2 from csvs
+					where csvs.winner_name = 'Roger Federer' AND
+					csvs.tourney_date ~ '^2018' AND
+					csvs.surface = 'Clay'
+				)
+		) AND csvs.winner_name = 'Roger Federer'
+	)kk
+)aa
+LIMIT 1
