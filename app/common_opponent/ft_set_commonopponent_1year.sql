daa5dec8ddaee47a0baf01f7301d953e85bff921
pleroux <p.leroux@gmx.com>
2018-10-28 13:22:00 +0100

    up

--- app/common_opponent/ft_set_commonopponent_1year.sql
+++ app/common_opponent/ft_set_commonopponent_1year.sql
@@ -7,8 +7,8 @@ CREATE OR REPLACE FUNCTION ft_set_commonopponent_1year()
      r record;
 	BEGIN
 		ALTER TABLE heads ADD COLUMN IF NOT EXISTS co1yearId varchar(255);
---        EXCEPTION
---            WHEN duplicate_column THEN RAISE NOTICE 'column co1yearId already exists in heads.';
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column co1yearId already exists in heads.';
 		FOR r in (SELECT heads.id as heads_id, atpworldtours.date as date, atpworldtours.winner as winner, atpworldtours.loser as loser FROM "heads"
 				INNER JOIN "atpworldtours" ON atpworldtours."hashId" = "heads"."atpWorldTourId")
 		LOOP
@@ -30,6 +30,8 @@ CREATE OR REPLACE FUNCTION ft_set_commonopponent_1year_year(var_year varchar(255
      r record;
 	BEGIN
 		ALTER TABLE heads ADD COLUMN IF NOT EXISTS co1yearId varchar(255);
+    EXCEPTION
+        WHEN duplicate_column THEN RAISE NOTICE 'column co1yearId already exists in heads.';
 		FOR r in (SELECT heads.id as heads_id, atpworldtours.date as date, atpworldtours.winner as winner, atpworldtours.loser as loser FROM "heads"
 				INNER JOIN "atpworldtours" ON atpworldtours."hashId" = "heads"."atpWorldTourId" and atpworldtours.year = var_year)
 		LOOP
