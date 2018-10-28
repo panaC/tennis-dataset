f45ae2a8c5441f1fafe9a7ea6377c93b147cc86e
pleroux <p.leroux@gmx.com>
2018-10-28 13:10:02 +0100

    up

--- app/common_opponent/script_select_co_1year.sh
+++ app/common_opponent/script_select_co_1year.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
 export PGPASSWORD=dataset1747
 
+psql -h 10.164.0.2 -U dataset -d dataset-dump1 -a -f create_table.sql
+
 psql -h 10.164.0.2 -U dataset -d dataset-dump1 -a -f ft_set_commonopponent_1year.sql
 
 for i in {2001..2018}; do
