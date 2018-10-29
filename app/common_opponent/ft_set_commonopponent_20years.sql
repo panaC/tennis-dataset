7cb092cc39c87af549df47fde14fde8d7b5e033b
pleroux <p.leroux@gmx.com>
2018-10-29 12:08:28 +0100

    add script for 20 years

--- app/common_opponent/ft_set_commonopponent_20years.sql
+++ app/common_opponent/ft_set_commonopponent_20years.sql
@@ -21,4 +21,25 @@ CREATE OR REPLACE FUNCTION ft_set_commonopponent_20years()
  $$
 LANGUAGE 'plpgsql';
 
-select ft_set_commonopponent_20years();
+
+CREATE OR REPLACE FUNCTION ft_set_commonopponent_20years_year(var_year varchar(255))
+ returns void
+ AS $$
+ DECLARE
+ 	 var_id record;
+     r record;
+	BEGIN
+		ALTER TABLE heads ADD COLUMN co20yearsId varchar(255);
+        EXCEPTION
+            WHEN duplicate_column THEN RAISE NOTICE 'column co20yearsId already exists in heads.';
+		FOR r in (SELECT heads.id as heads_id, atpworldtours.date as date, atpworldtours.winner as winner, atpworldtours.loser as loser FROM "heads"
+				INNER JOIN "atpworldtours" ON atpworldtours."hashId" = "heads"."atpWorldTourId" and atpworldtours.year = var_year)
+		LOOP
+			insert into commonopponents select * from ft_diff_co(20, r.date::date, r.winner, r.loser)
+				returning id into var_id;
+			update heads set co20yearsId = var_id where heads.id = r.heads_id;
+		END LOOP;
+		RETURN;
+	END;
+ $$
+LANGUAGE 'plpgsql';
---
+++ app/common_opponent/script_select_co_20years.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+export PGPASSWORD=dataset1747
+
+psql -h 10.164.0.2 -U dataset -d dataset-dump1 -a -f create_table.sql
+
+psql -h 10.164.0.2 -U dataset -d dataset-dump1 -a -f ft_set_commonopponent_20years.sql
+
+for i in {2001..2018}; do
+  echo "SELECT ft_set_commonopponent_20years_year('$i');" | psql -h 10.164.0.2 -U dataset -d dataset-dump1 -a &
+done
